9e6c6a8feae88841accc6dbf2203c244976b3f1b
1realjulius <iamjuliuseghan@gmail.com>
2022-06-17 03:59:49 +0000

    Initial commit

---
+++ _next/static/7SijlZZYpp23MxO4P4Tt1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/650-3f667c3c3b7bdb5d.js","static/chunks/pages/index-d808739b2464f045.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
